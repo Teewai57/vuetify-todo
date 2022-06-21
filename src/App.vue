e5514f486698a848a3104022b49c704b5eb9df0a
Teewai57 <bobtoyoabasi@gmail.com>
2022-06-21 23:00:10 +0100

    added drawer function

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,20 @@
       </v-app-bar-nav-icon>
 
         <v-toolbar-title>Application
-          
+
         </v-toolbar-title>
 
     </v-app-bar>
+
+    <v-main>
+
+    </v-main>
   </v-app>
 
 </template>
+
+<script>
+  export default {
+    data: () => [{drawer: null}],
+  }
+</script>
